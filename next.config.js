ab647423fe72d0671fe7d026752450fa6d7167af
KARL NASSAR <kgnassar01@gmail.com>
2021-06-21 22:46:45 +0300

    homepage+api

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["pngimg.com", "fakestoreapi.com"],
+  },
+};
---
+++ src/components/ProductFeed.js
@@ -0,0 +1,60 @@
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        className="md:col-span-full"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/SBP/2018/gateway/1110572_smb_gw_desktop_1500x300_lavolio_1x_uk._CB484123630_.jpg"
+        alt="amazon ad"
+      />
+
+      <div className="md:col-span-2 xl:col-span-3">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default ProductFeed;
